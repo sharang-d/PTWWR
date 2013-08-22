2889d239b490c7eb95faf5d8161dfdf00ae46c3c
sharang-d <sharang.d@gmail.com>
2013-08-22 21:26:59 +0530

    Used the lambda operator

--- 1week/6day/my_rack_proc.rb
+++ 1week/6day/my_rack_proc.rb
@@ -1,2 +1,2 @@
-my_rack_proc = lambda { |env| [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{ARGV[0]}"]] }
+my_rack_proc = ->(env){ [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{ARGV[0]}"]] }
 puts my_rack_proc[{}] if ARGV[0]
