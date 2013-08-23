e750b99202cc1a4fbdba74b3dfb6cc68f112f213
sharang-d <sharang.d@gmail.com>
2013-08-23 15:57:03 +0530

    Changed :Port => 8500 to Port: 8500

--- 1week/7day/my_rack_proc.rb
+++ 1week/7day/my_rack_proc.rb
@@ -1,3 +1,3 @@
 require 'rack'
 my_rack_proc = ->(env){ [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{ARGV[0]}"]] }
-ARGV[0] ? Rack::Handler::WEBrick.run(my_rack_proc, :Port => 8500) : puts('Please supply a command line argument')
+ARGV[0] ? Rack::Handler::WEBrick.run(my_rack_proc, Port: 8500) : puts('Please supply a command line argument')
